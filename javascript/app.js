c3736c3714fa14e618e53bde345ee6eea59fac22
Francisco Marin <franjmarin1991@gmail.com>
2022-03-01 14:24:43 -0300

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,50 @@
+const cardArray = [
+    {
+        name: 'eren',
+        img: 'images/eren-jeager.png'
+    },
+    {
+        name: 'armin',
+        img: 'images/armin-arlert.png'
+    },
+    {
+        name: 'mikasa',
+        img: 'images/mikasa-ackerman.png'
+    },
+    {
+        name: 'levi',
+        img: 'images/levi-ackerman.png'
+    },
+    {
+        name: 'reiner',
+        img: 'images/reiner-braun.png'
+    },
+    {
+        name: 'zeke',
+        img: 'images/zeke-jeager.png'
+    },
+    {
+        name: 'eren',
+        img: 'images/eren-jeager.png'
+    },
+    {
+        name: 'armin',
+        img: 'images/armin-arlert.png'
+    },
+    {
+        name: 'mikasa',
+        img: 'images/mikasa-ackerman.png'
+    },
+    {
+        name: 'levi',
+        img: 'images/levi-ackerman.png'
+    },
+    {
+        name: 'reiner',
+        img: 'images/reiner-braun.png'
+    },
+    {
+        name: 'zeke',
+        img: 'images/zeke-jeager.png'
+    },
+];
